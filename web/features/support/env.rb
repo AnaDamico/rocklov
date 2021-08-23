008f00a03bc272d3d6d36bdea10c20b2454928cb
Ana Carolina <anac.damico@gmail.com>
2021-08-22 21:20:18 -0300

    Adicionando script Jenkins para web

--- web/features/support/env.rb
+++ web/features/support/env.rb
@@ -13,6 +13,18 @@ when "firefox_headless"
 when "chrome"
     @driver = :selenium_chrome
 when "chrome_headless"
+
+    Capybara.register_driver :selenium_chrome_headless do |app|
+        version = Capybara::Selenium::Driver.load_selenium
+        options_key = Capybara::Selenium::Driver::CAPS_VERSION.satisfied_by?(version) ? :capabilities : :options
+        browser_options = ::Selenium::WebDriver::Chrome::Options.new.tap do |opts|
+          opts.add_argument('--headless')
+          opts.add_argument('--disable-gpu')
+          opts.add_argument('--disable-site-isolation-trials')
+          opts.add_argument('--no-sandbox')
+          opts.add_argument('--disable-dev-shm-usage')
+        end
+
     @driver = :selenium_chrome_headless
 else
     raise "Navegador Incorreto, variável @driver está vazia !!"
